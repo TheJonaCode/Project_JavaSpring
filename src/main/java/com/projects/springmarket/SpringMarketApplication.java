c739b6d2126fb148c225f551807eca4628fdb8b4
Jonathan <mcjhona069@gmail.com>
2021-03-01 11:28:45 -0600

    Inicialización del repositorio

---
+++ src/main/java/com/projects/springmarket/SpringMarketApplication.java
@@ -0,0 +1,13 @@
+package com.projects.springmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMarketApplication.class, args);
+	}
+
+}
