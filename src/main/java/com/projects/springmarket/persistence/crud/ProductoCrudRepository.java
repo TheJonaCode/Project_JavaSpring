93bf4d8278d0966df87e0156e56edcdeb9e60f30
Jonathan <mcjhona069@gmail.com>
2021-03-10 16:27:04 -0600

    Query Metods

--- src/main/java/com/projects/springmarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/projects/springmarket/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,14 @@
 package com.projects.springmarket.persistence.crud;
 
 import com.projects.springmarket.persistence.entity.Producto;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLlessThanAndEstado(int cantidadStock);
 }
