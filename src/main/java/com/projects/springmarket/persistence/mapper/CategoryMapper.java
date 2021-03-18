8898e1c1c69c20eeeba2a176452a3a2bdd54e17d
Jonathan <mcjhona069@gmail.com>
2021-03-18 12:47:46 -0600

    Orientar nuestra API al dominio MapStruct

---
+++ src/main/java/com/projects/springmarket/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package com.projects.springmarket.persistence.mapper;
+
+import com.projects.springmarket.domain.Category;
+import com.projects.springmarket.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoriaId"),
+            @Mapping(source = "descripcion", target = "category"),
+            @Mapping(source = "estado", target = "active"),
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productos", ignore = true)
+    Categoria toCategoria(Category category);
+}
---
+++ src/main/java/com/projects/springmarket/persistence/mapper/ProductMapper.java
@@ -0,0 +1,29 @@
+package com.projects.springmarket.persistence.mapper;
+
+import com.projects.springmarket.domain.Product;
+import com.projects.springmarket.persistence.entity.Producto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
+public interface ProductMapper {
+    @Mappings({
+            @Mapping(source = "idProducto", target = "productId"),
+            @Mapping(source = "nombre", target = "name"),
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "precioVenta", target = "price"),
+            @Mapping(source = "cantidadStock", target = "stock"),
+            @Mapping(source = "estado", target = "active"),
+            @Mapping(source = "categoria", target = "category"),
+    })
+    Product toProduct(Producto producto);
+    List<Product> toProducts(List<Producto> productos);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "codigoBarrras", ignore=true)
+    Producto toProducto(Product product);
+}
