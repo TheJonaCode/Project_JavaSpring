82c82e47b9af61358c49c15427c8a3f96a4cb11b
Jonathan <mcjhona069@gmail.com>
2021-03-22 10:08:07 -0600

    Implementar la anotación @Service

---
+++ src/main/java/com/projects/springmarket/domain/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.projects.springmarket.domain.service;
+
+import com.projects.springmarket.domain.Product;
+import com.projects.springmarket.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll(){
+        return productRepository.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId){
+        return productRepository.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId){
+        return productRepository.getByCategory(categoryId);
+    }
+
+    public Product save(Product product){
+        return productRepository.save(product);
+    }
+
+    public boolean delete(int productId){
+        return getProduct(productId).map(product ->{
+            productRepository.delete(productId);
+            return true;
+        }).orElse(false);
+    }
+}
