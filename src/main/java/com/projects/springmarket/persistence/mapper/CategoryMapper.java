3b2216decab5df32ffe55126a00e8cfe6efd186a
Jonathan <mcjhona069@gmail.com>
2021-03-22 10:24:19 -0600

    Implementar la anotacion @RestController

--- src/main/java/com/projects/springmarket/persistence/mapper/CategoryMapper.java
+++ src/main/java/com/projects/springmarket/persistence/mapper/CategoryMapper.java
@@ -10,7 +10,7 @@ import org.mapstruct.Mappings;
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
     @Mappings({
-            @Mapping(source = "idCategoria", target = "categoriaId"),
+            @Mapping(source = "idCategoria", target = "categoryId"),
             @Mapping(source = "descripcion", target = "category"),
             @Mapping(source = "estado", target = "active"),
     })
