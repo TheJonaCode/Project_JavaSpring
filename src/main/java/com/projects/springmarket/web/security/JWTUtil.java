0cb6976879ef45aad765818c24212d4269bbd6bd
Jonathan <mcjhona069@gmail.com>
2021-03-24 11:08:49 -0600

    Autenticación con JWT

--- src/main/java/com/projects/springmarket/web/security/JWTUtil.java
+++ src/main/java/com/projects/springmarket/web/security/JWTUtil.java
@@ -3,14 +3,16 @@ package com.projects.springmarket.web.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+@Component
 public class JWTUtil {
 
     private static final String KEY = "platzi";
 
-    public String generateTOoken(UserDetails userDetails) {
+    public String generateToken(UserDetails userDetails) {
         return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, KEY).compact();
