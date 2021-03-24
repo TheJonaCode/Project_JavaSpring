97774b2d561d479ba3ddc95eeb93b5bee5ef66ed
Jonathan <mcjhona069@gmail.com>
2021-03-24 10:55:40 -0600

    Generar un JWT

---
+++ src/main/java/com/projects/springmarket/web/security/JWTUtil.java
@@ -0,0 +1,18 @@
+package com.projects.springmarket.web.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
+public class JWTUtil {
+
+    private static final String KEY = "platzi";
+
+    public String generateTOoken(UserDetails userDetails) {
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, KEY).compact();
+    }
+}
